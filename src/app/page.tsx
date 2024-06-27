3ba312574baf7f7383a03de1b7935f34bbf3840b
thoulee <thou_lee@outlook.com>
2024-06-27 21:22:31 +0800

    feat: Update global CSS styles and page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,25 @@
 'use client'
 import styles from './page.module.css';
 
+import { Header } from '@/components/Header';
 import { ContentEditable, EditableProvider, withEditable } from "@editablejs/editor";
-import { Transforms, createEditor } from "@editablejs/models";
+import { createEditor } from "@editablejs/models";
 import { ContextMenu, useContextMenuEffect, withContextMenu } from '@editablejs/plugin-context-menu';
 import { withHistory } from '@editablejs/plugin-history';
-import { Toolbar, ToolbarComponent, useToolbarEffect, withToolbar } from '@editablejs/plugin-toolbar';
+import { Toolbar as EditableToolbar, ToolbarComponent, useToolbarEffect, withToolbar } from '@editablejs/plugin-toolbar';
 import { MarkEditor, MarkFormat, withPlugins } from '@editablejs/plugins';
 import CodeIcon from '@mui/icons-material/Code';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import ContentPasteIcon from '@mui/icons-material/ContentPaste';
-import FileUploadIcon from '@mui/icons-material/FileUpload';
 import FormatBoldIcon from '@mui/icons-material/FormatBold';
 import FormatItalicIcon from '@mui/icons-material/FormatItalic';
 import FormatStrikethroughIcon from '@mui/icons-material/FormatStrikethrough';
 import FormatUnderlinedIcon from '@mui/icons-material/FormatUnderlined';
 import SubscriptIcon from '@mui/icons-material/Subscript';
 import SuperscriptIcon from '@mui/icons-material/Superscript';
+import Container from '@mui/material/Container';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { useMemo } from "react";
 import { withDocx } from '../utils/docx/withDocx';
 
@@ -69,13 +72,21 @@ export default function Home() {
   }, [])
 
   useToolbarEffect(() => {
-    Toolbar.setItems(editor, marks.map(mark => ({
+    EditableToolbar.setItems(editor, marks.map(mark => ({
       type: 'button',
       active: MarkEditor.isActive(editor, mark.name),
-      onToggle: () => {
-        MarkEditor.toggle(editor, mark.name)
-      },
-      icon: mark.icon,
+      content: (
+        <ToggleButton
+          key={mark.name}
+          value={mark.name}
+          selected={MarkEditor.isActive(editor, mark.name)}
+          onClick={() => {
+            MarkEditor.toggle(editor, mark.name)
+          }}
+        >
+          {mark.icon}
+        </ToggleButton>
+      )
     })))
   }, editor)
 
@@ -96,42 +107,28 @@ export default function Home() {
         onSelect: () => {
           editor.insertFromClipboard()
         }
-      },
-      {
-        key: 'upload',
-        title: '上传',
-        icon: <FileUploadIcon />,
-        content:
-          <div>
-            <input
-              type="file"
-              accept=".docx"
-              onChange={async (event) => {
-                const fileObj = event.target.files && event.target.files[0];
-                if (!fileObj) {
-                  return;
-                }
-
-                const reader = new FileReader();
-                reader.readAsArrayBuffer(fileObj);
-
-                reader.onload = function () {
-                  Transforms.deselect(editor);
-                  //@ts-expect-error
-                  editor.loadDocx(this.result);
-                }
-              }}
-            />
-          </div>
       }
     ])
   }, editor)
 
   return (
-    <div className={styles.main}>
+    <div className={styles.root}>
       <EditableProvider editor={editor}>
-        <ToolbarComponent editor={editor} className={styles.toolbar} />
-        <ContentEditable placeholder="Waiting for input..." />
+        <Header />
+        <div className={styles.toolbar}>
+          <ToggleButtonGroup
+            color='primary'
+            size='small'
+          >
+            <ToolbarComponent editor={editor} />
+          </ToggleButtonGroup>
+        </div>
+
+        <Container maxWidth="md">
+          <div className={styles.main}>
+            <ContentEditable placeholder="Waiting for input..." />
+          </div>
+        </Container>
       </EditableProvider>
     </div>
   );
