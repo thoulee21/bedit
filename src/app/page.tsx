5a88fea1de3b0ea0624f2d40f5715bdb577c9ed7
thoulee <thou_lee@outlook.com>
2024-06-30 23:46:40 +0800

    feat: add editable playground's menus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import styles from './page.module.css';
 
 import { Header } from '@/components/Header';
-import { marks } from '@/utils/mark';
+import { createToolbarItems } from '@/configs/toolbar-items';
 import { ContentEditable, EditableProvider, withEditable } from "@editablejs/editor";
 import { createEditor } from "@editablejs/models";
 import { ContextMenu, useContextMenuEffect, withContextMenu } from '@editablejs/plugin-context-menu';
@@ -14,15 +14,13 @@ import {
   useToolbarEffect,
   withToolbar
 } from '@editablejs/plugin-toolbar';
-import { MarkEditor, withPlugins } from '@editablejs/plugins';
+import { withPlugins } from '@editablejs/plugins';
 import { Icon } from '@editablejs/ui';
 import { CloudQueueRounded } from '@mui/icons-material';
-import { Paper, Snackbar } from '@mui/material';
-import Container from '@mui/material/Container';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import { Paper, Snackbar, Container } from '@mui/material';
 import { useMemo, useState } from "react";
 import { withDocx } from '../utils/docx/withDocx';
+import { createContextMenuItems } from '@/configs/context-menu-items';
 
 export default function Home() {
   const [snackbarOpen, setSnackbarOpen] = useState(false)
@@ -38,84 +36,48 @@ export default function Home() {
     return editable
   }, [])
 
+  const askAI = async () => {
+    if (editor.selection) {
+      try {
+        setSnackbarOpen(true)
+        const fragment = editor.getFragment(editor.selection)
+        //@ts-expect-error
+        const selected = fragment[0].children[0].text
+        const resp = await fetch(`http://localhost:8080/chat?prompt=${selected}&stream=true`)
+        const aiTxt = await resp.text()
+
+        setSnackbarOpen(false)
+        editor.insertNode({ type: 'paragraph', children: [{ text: aiTxt }] })
+      }
+      catch (e) {
+        console.info(e)
+      } finally {
+        setSnackbarOpen(false)
+      }
+    }
+  }
+
   useToolbarEffect(() => {
-    EditableToolbar.setItems(editor, marks.map(mark => ({
-      type: 'button',
-      active: MarkEditor.isActive(editor, mark.name),
-      content: (
-        <ToggleButton
-          key={mark.name}
-          value={mark.name}
-          selected={MarkEditor.isActive(editor, mark.name)}
-          onClick={() => {
-            MarkEditor.toggle(editor, mark.name)
-          }}
-        >
-          {mark.icon}
-        </ToggleButton>
-      )
-    })))
+    EditableToolbar.setItems(editor, createToolbarItems(editor))
   }, editor)
 
   useContextMenuEffect(() => {
-    ContextMenu.setItems(editor, [
-      {
-        key: 'copy',
-        title: '复制',
-        icon: <Icon name='copy' />,
-        rightText: 'Ctrl+C',
-        onSelect: () => {
-          editor.copy()
-        }
-      },
-      {
-        key: 'paste',
-        title: '粘贴',
-        icon: <Icon name='paste' />,
-        rightText: 'Ctrl+V',
-        onSelect: () => {
-          editor.insertFromClipboard()
-        }
-      },
-      {
-        key: 'ai',
-        title: 'Ask AI',
-        icon: <Icon name='blockquote' />,
-        onSelect: async () => {
-          if (editor.selection) {
-            try {
-              setSnackbarOpen(true)
-              const fragment = editor.getFragment(editor.selection)
-              //@ts-expect-error
-              const selected = fragment[0].children[0].text
-              const resp = await fetch(`http://localhost:8080/chat?prompt=${selected}&stream=true`)
-              const aiTxt = await resp.text()
+    const contextMenu = createContextMenuItems(editor)
+    contextMenu.push({
+      key: 'ai',
+      title: 'Ask AI',
+      icon: <Icon name='blockquote' />,
+      onSelect: askAI
+    })
 
-              setSnackbarOpen(false)
-              editor.insertText(aiTxt)
-            }
-            catch (e) {
-              console.info(e)
-            } finally {
-              setSnackbarOpen(false)
-            }
-          }
-        }
-      }
-    ])
+    ContextMenu.setItems(editor, contextMenu)
   }, editor)
 
   return (
     <div className={styles.root}>
       <EditableProvider editor={editor}>
         <Header />
-        <ToggleButtonGroup
-          color='primary'
-          size='small'
-          className={styles.toolbar}
-        >
-          <ToolbarComponent editor={editor} />
-        </ToggleButtonGroup>
+        <ToolbarComponent editor={editor} className={styles.toolbar} />
 
         <Container maxWidth="md" className={styles.main}>
           <ContentEditable placeholder="Waiting for input..." />
